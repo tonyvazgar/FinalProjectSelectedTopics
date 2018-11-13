32a0ab9ff0090120b650a81717e671727c4e91fd
tonyvazgar <luis.vazquezga@udlap.mx>
2018-11-12 18:12:37 -0600

    first commit

---
+++ FinalProjectSelectedTopics/FinalProjectSelectedTopics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalProjectSelectedTopics
+//
+//  Created by Tony Vazgar on 11/12/18.
+//  Copyright © 2018 Tony Vazgar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
